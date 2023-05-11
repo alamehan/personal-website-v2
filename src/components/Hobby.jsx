aa1aa875639a9d6e6cb111c203c80422025cc60f
alamehan <raifalam98@gmail.com>
2023-05-11 23:33:21 +0700

    [UPDATE] Hobby links

--- src/components/Hobby.jsx
+++ src/components/Hobby.jsx
@@ -27,18 +27,18 @@ const Hobby = () => {
 					<CardHobby image={hobbyAnggrekClassifier} alt="Anggrek Classifier" link="https://github.com/alamehan/skripsi-cnn-anggrek" classCard="bg-brown-100" title="Anggrek Classifier" description="Orchid plant genus classification using Deep Learning ConvNets." />
 					<CardHobby image={hobbyNiceVizual} alt="NiceVizual" link="https://nicevizual.github.io/" classCard="bg-purple-100" title="NiceVizual" description="A collection of simple interactive apps based on HTML, CSS & JavaScript." />
 					<CardHobby image={hobbySatuKlik} alt="SatuKlik" link="https://satuklik.github.io/" classCard="bg-sky-100" title="SatuKlik" description="All your links are collected into one web page. More than 20 themes are available." />
-					<CardHobby image={hobbyCheatSheet} alt="CheatSheet" link="https://alamehan.github.io/sheet/" classCard="bg-blue-100" title="CheatSheet" description="A ready-to-use reference, presented in a fun style with engaging illustrations." />
+					<CardHobby image={hobbyCheatSheet} alt="CheatSheet" link="https://alamehan.github.io/alamehan-v1/sheet/" classCard="bg-blue-100" title="CheatSheet" description="A ready-to-use reference, presented in a fun style with engaging illustrations." />
 					<CardHobby image={hobbyNiceList} alt="NiceList" link="https://github.com/alamehan/nice-list" classCard="bg-violet-100" title="NiceList" description="A list of quality and useful websites and digital products in one place." />
-					<CardHobby image={hobbyTheReaderrr} alt="The Readerrr" link="https://alamehan.github.io/epub/" classCard="bg-slate-100" title="The Readerrr" description="Curated for book lovers. Useful tools, book recommendations, and selected reading lists." />
-					<CardHobby image={hobbyAtozMind} alt="ATOZ Mind" link="https://alamehan.github.io/mind-v2/" classCard="bg-pink-100" title="ATOZ Mind" description="A collection of mind maps on various topics. See the Big Picture!" />
-					<CardHobby image={hobbyRightTr4ck} alt="Right Tr4ck" link="https://alamehan.github.io/road/" classCard="bg-orange-100" title="Right Tr4ck" description="A learning framework. Want to learn something new and don't know where to start?" />
+					<CardHobby image={hobbyTheReaderrr} alt="The Readerrr" link="https://alamehan.github.io/alamehan-v1/epub/" classCard="bg-slate-100" title="The Readerrr" description="Curated for book lovers. Useful tools, book recommendations, and selected reading lists." />
+					<CardHobby image={hobbyAtozMind} alt="ATOZ Mind" link="https://alamehan.github.io/alamehan-v1/mind-v2/" classCard="bg-pink-100" title="ATOZ Mind" description="A collection of mind maps on various topics. See the Big Picture!" />
+					<CardHobby image={hobbyRightTr4ck} alt="Right Tr4ck" link="https://alamehan.github.io/alamehan-v1/road/" classCard="bg-orange-100" title="Right Tr4ck" description="A learning framework. Want to learn something new and don't know where to start?" />
 					<CardHobby image={hobbyKnowYourself101} alt="Know Yourself 101" link="https://drive.google.com/drive/u/3/folders/1q2XKCioGNsoqeyc-NfiG0BcaAXIfwCwD" classCard="bg-lime-100" title="Know Yourself 101" description="Find out who you really are. All great achievements start with knowing yourself." />
 
 					{/* ---------------------------- Show More Content --------------------------- */}
 
 					{showMore && (
 						<>
-							<CardHobby image={hobbyDesignShowcase} alt="Design Showcase" link="https://alamehan.github.io/portfolios.html" classCard="bg-indigo-100" title="Design Showcase" description="Design exploration such as digital illustration, logo design, playing with color, etc." />
+							<CardHobby image={hobbyDesignShowcase} alt="Design Showcase" link="https://alamehan.github.io/alamehan-v1/portfolios.html" classCard="bg-indigo-100" title="Design Showcase" description="Design exploration such as digital illustration, logo design, playing with color, etc." />
 							<CardHobby image={hobbyInsightStories} alt="Insight Stories" link="https://alamehan.medium.com/selamat-datang-di-alamehan-35a43da9d87e" classCard="bg-slate-100" title="Insight Stories" description="Sharing insights, book reviews, and stories about life. Read Raihan's stories here." />
 							<CardHobby image={hobbyPetualanganOemar} alt="Petualangan Oemar" link="http://petualanganoemar.blogspot.com/" classCard="bg-sky-100" title="Petualangan Oemar" description="A simple game created using the RPG maker engine in 2012. An archive." />
 						</>
